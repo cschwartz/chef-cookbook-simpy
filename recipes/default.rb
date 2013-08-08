df7bc9e1af928f3ec3834878933e0aa8d341b7ac
christian.schwartz@gmail.com <christian.schwartz@gmail.com>
2013-08-08 15:54:09 +0200

    refactor

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-user "simpy" do
+simpy_user = "simpy"
+simpy_home = File.join("/", "home", simpy_user)
+
+user simpy_user do
   supports manage_home: true
   shell "/bin/bash"
-  home "/home/simpy"
+  home simpy_home
 end
 
 
@@ -19,54 +22,65 @@ include_recipe "pypy::pip"
 
 pypy_pip "virtualenv"
 
-pypy_virtualenv "/home/simpy/simpy-env" do
+simpy_virtualenv = File.join(simpy_home, "simpy-env")
+
+pypy_virtualenv simpy_virtualenv do
   interpreter "pypy"
-  owner "simpy"
-  group "simpy"
+  owner simpy_user
+  group simpy_user
 end
 
 package "mercurial"
 
 pypy_pip "hg+https://bitbucket.org/simpy/simpy" do
-  virtualenv "/home/simpy/simpy-env"
-  user "simpy"
+  virtualenv simpy_virtualenv
+  user simpy_user
 end
 
 gem_package "bundler"
 
 package "git"
 
-directory "/home/simpy/.ssh" do
-  owner "simpy"
+simpy_ssh_directory = File.join(simpy_home, ".ssh")
+
+directory simpy_ssh_directory do
+  owner simpy_user
 end
 
-file "/home/simpy/.ssh/deploy_key" do
+simpy_deploy_key = File.join(simpy_ssh_directory, "deploy_key")
+vagrant_deploy_key_location = File.join("/", "vagrant", "deploy_key")
+metal_deploy_key_location = File.join("/", "tmp", "deploy_key")
+
+file simpy_deploy_key do
+  is_vagrant = File.exists?("/vagrant") && File.directory?("/vagrant")
   mode 00600
-  owner "simpy"
-  if File.exists?("/vagrant") && File.directory?("/vagrant")
-    content File.read("/vagrant/deploy_key")
-  else
-    content File.read("/tmp/deploy_key")
-  end
+  owner simpy_user
+  content File.read is_vagrant ? vagrant_deploy_key_location : metal_deploy_key_location
 end
 
-file "/home/simpy/deploy_ssh" do
+simpy_ssh_wrapper = File.join(simpy_home, "deploy_ssh")
+
+file simpy_ssh_wrapper do
   content <<-eos
 #!/bin/sh
-exec /usr/bin/ssh -o StrictHostKeyChecking=no -i /home/simpy/.ssh/deploy_key "$@"
+exec /usr/bin/ssh -o StrictHostKeyChecking=no -i #{ simpy_deploy_key } "$@"
 eos
   mode 00755
-  owner "simpy"
+  owner simpy_user
 end
 
-git "/home/simpy/ggsn-models" do
-  user "simpy"
+simpy_project_directory = File.join(simpy_home, "ggsn-models")
+
+git simpy_project_directory do
+  user simpy_user
   repository "git@github.com:fmetzger/ggsn-models.git"
-  ssh_wrapper "/home/simpy/deploy_ssh"
+  ssh_wrapper simpy_ssh_wrapper
 end
+
+simpy_simulation_directory = File.join(simpy_project_directory, "simulation")
 
 bash "bundle install for simulation" do
-  user "simpy"
-  cwd "/home/simpy/ggsn-models/simulation"
+  user simpy_user
+  cwd simpy_simulation_directory
   code "bundle install --path vendor"
 end
